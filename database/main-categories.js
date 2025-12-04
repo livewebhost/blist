820c2a13bbee5f2223a5464fb5be678bcfad1173
livewebhost <sandeepkashyap.marketing@gmail.com>
2025-12-05 03:14:42 +0530

    Create main-categories.js

---
+++ database/main-categories.js
@@ -0,0 +1,30 @@
+const products = [
+    { name: "Daily Home Needs", link: "daily-home-needs.html", groups: 7 },
+  ];
+
+  const container = document.getElementById("catagories-sec-inner");
+
+  // Remove duplicates by link
+  const uniqueProducts = products.filter(
+    (product, index, self) =>
+      index === self.findIndex(p => p.link === product.link)
+  );
+
+  // Sort by groups (descending)
+  uniqueProducts.sort((a, b) => b.groups - a.groups);
+
+  // Append HTML blocks
+  uniqueProducts.forEach(product => {
+    const div = document.createElement("div");
+    div.className = "i-box-s1";
+    div.innerHTML = `
+      <div class="thumb">
+        <img src="assets/images/categoryicons/facebook_groups_category.png" alt="" />
+      </div>
+      <div class="content">
+        <h3>${product.name} (${product.groups})</h3>
+        <a href="${product.link}" class="f-btn">View All<i class="fa-solid fa-arrow-right"></i></a>
+      </div>
+    `;
+    container.appendChild(div);
+  });
